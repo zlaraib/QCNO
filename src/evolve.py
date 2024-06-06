37a900990b49886b578033d69401eff6f4c96fd7
zlaraib <zlaraib@vols.utk.edu>
2024-06-06 12:55:43 -0400

    added modular file for evolve and measure function in src code directory

---
+++ src/evolve.py
@@ -0,0 +1,16 @@
+# evolve.py 
+
+import numpy as np
+from scipy.linalg import expm
+from qiskit import execute
+from qiskit.circuit import QuantumCircuit
+from qiskit.quantum_info import Operator
+
+def evolve_and_measure_circuit(time, H, N_sites):
+    U = Operator(expm(-1j * H * time))
+    qc = QuantumCircuit(N_sites, 1)
+    half_N_sites = N_sites // 2
+    qc.x(range(half_N_sites))
+    qc.unitary(U, range(N_sites), label="exp(-iHt)")
+    qc.measure(0, 0)
+    return qc
