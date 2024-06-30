8d471f47a31391cc5001e397d920e2546a3d5951
zlaraib <zlaraib@vols.utk.edu>
2024-06-30 12:12:04 -0400

    added the function that generates the quantum circuit specifically for richers. needs generalization

--- src/evolve.py
+++ src/evolve.py
@@ -15,34 +15,38 @@ def evolve_and_measure_circuit(time, H, N_sites):
     qc.x(range(half_N_sites))
     qc.unitary(U, range(N_sites), label="exp(-iHt)")
     qc.measure(0, 0)
+    # print(qc.decompose(reps=2)) #reps repeats the deocmpostion that many times. 
     return qc
 
-# def store_vals()
-#     # Create empty arrays to store the respective values
-#     Sz_array = []
-#     Sy_array = []
-#     Sx_array = []
-#     t_array = []
-#     prob_surv_array = []
-#     x_values = []
-#     px_values = []
-#     ρₑₑ_array = []
-#     ρ_μμ_array = []
-#     ρₑμ_array = []
 
-#     # Initialize variables to store ρₑμ at t1 and t2
-#     ρₑμ_at_t1 = None
-#     ρₑμ_at_t2 = None
+def evolve_and_measure_qcircuit(time, H, N_sites, measure='Z'):
+    U = Operator(expm(-1j * H * time* 1/hbar))
+    
+    qc = QuantumCircuit(N_sites, 1)
+    half_N_sites = N_sites // 2
+    # qc.x(range(half_N_sites))
+    
+    # Initialize the first half of the chain as up-spin (|0>)
+    # Initialize the second half of the chain as down-spin (|1>)
+    for i in range(half_N_sites, N_sites):
+        qc.x(i)
+        
+    qc.unitary(U, range(N_sites), label="exp(-iHt)")
+    
+    # transform basis from Z (up/down or |0>/|1>) to X (equal super position of |up>/ |down> to get the x basis =|+>/|->) using hadamard on the first qubit(b/c we are measurng the first qubit only)
+    # the measurement of counts in x basis is done later in main script to compute <sigma_x>
+    if measure == 'X':
+        qc.h(0)
+        
+    # prepare the quantum state by transforming basis from Z to Y using sdg on the first qubit(b/c we are measurng the first qubit only)
+    # Similarly, the 'Sdg' gate (S-dagger) is applied, which corresponds to the inverse of the phase gate 'S'. This introduces a phase shift of - pi/2.
+    # Then, a Hadamard gate 'H' is applied. This sequence of gates transforms from Z basis to Y basis. 
+    
+    # the measurement of counts in Y basis is done later in main script to compute <sigma_y>
+    elif measure == 'Y':
+        qc.sdg(0)
+        qc.h(0)
 
-#     # Example values for t1 and t2 (you can adjust these as needed)
-#     t1 = 0.0
-#     t2 = 1.0
-#     Δt = t2 - t1  # Time difference between growth rates
-
-#     # Extract output of p_hat and p_mod for the p vector defined above for all sites
-#     p_mod, p_hat = momentum(p, N_sites)
-
-#     # Extract px values from p_hat
-#     px_values = [sub_array[0] for sub_array in p_hat]
-#     return Sz_array, Sy_array, Sx_array, prob_surv_array, x_values, px_values, ρₑₑ_array, ρ_μμ_array, ρₑμ_array
+    qc.measure(0, 0)
+    return qc
 
