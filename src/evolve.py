ba93a527c160e717e9c512580ba7dfa64cc5ee54
zlaraib <zlaraib@vols.utk.edu>
2024-08-06 21:53:29 -0400

    changed the evolve function such that it includes the measurement in y and x basis and has trotterization embedded and also handles the richers vs non richers tests compatibilities via hbar incorporation and initial state variations

--- src/evolve.py
+++ src/evolve.py
@@ -29,12 +29,42 @@ def apply_two_qubit_gate(qc, coef, qubit1, qubit2, pauli1, pauli2):
         qc.rzz(2 * coef, qubit1, qubit2)
 
 
-def evolve_and_measure_circuit(time, pauli_terms, N_sites,trotter_order):
-    qc = QuantumCircuit(N_sites, 1)
-    half_N_sites = N_sites // 2
-    qc.x(range(half_N_sites))
+# def evolve_and_measure_circuit(time, pauli_terms, N_sites,trotter_order):
+#     qc = QuantumCircuit(N_sites, 1)
+#     half_N_sites = N_sites // 2
+#     qc.x(range(half_N_sites))
 
-    num_steps = 10  # Number of Trotter steps
+#     num_steps = 10 # Number of Trotter steps
+#     dt = time / num_steps
+
+#     if trotter_order == 'first':
+#         dt_substep = dt
+#     if trotter_order == 'second':
+#         dt_substep = dt/2
+#         pauli_terms = pauli_terms + pauli_terms[::-1]
+        
+#     for _ in range(num_steps):
+#         for coef, pauli in pauli_terms:
+#             #converts the Pauli operator object into its string representation
+#             pauli_str = pauli.to_label()
+            
+#             #Creates a list of qubits that are affected by the Pauli term.
+#             #uses a list comprehension that iterates over the string representation pauli_str of the Pauli operator.
+#             # enumerate(pauli_str) provides both the index i and the character p for each position in the string.
+#             #the condition if p != 'I' ensures that only qubits with a Pauli operator other than the identity I are included in the list qubits.
+#             qubits = [i for i, p in enumerate(pauli_str) if p != 'I']
+            
+#             if len(qubits) == 1:
+#                 #coef * dt_substep: The rotation angle, which is the coefficient coef multiplied by the time step dt_substep
+#                 apply_single_qubit_gate(qc, coef * dt_substep, qubits[0], pauli_str[qubits[0]])
+#             elif len(qubits) == 2:
+#                 apply_two_qubit_gate(qc, coef * dt_substep, qubits[0], qubits[1], pauli_str[qubits[0]], pauli_str[qubits[1]])
+
+#     qc.measure(0, 0)
+#     return qc
+
+def evolve_and_measure_circuit(time, pauli_terms,  N_sites, theta_nu, trotter_order, measure='Z'):
+    num_steps = 10 # Number of Trotter steps
     dt = time / num_steps
 
     if trotter_order == 'first':
@@ -43,6 +73,31 @@ def evolve_and_measure_circuit(time, pauli_terms, N_sites,trotter_order):
         dt_substep = dt/2
         pauli_terms = pauli_terms + pauli_terms[::-1]
         
+    
+    qc = QuantumCircuit(N_sites, 1)
+    half_N_sites = N_sites // 2
+    if theta_nu == 1.74532925E-8 : 
+        dt_substep = dt_substep/hbar # since the unitary is divided by hbar in richers test only 
+        for i in range(half_N_sites, N_sites):
+            qc.x(i) # inital state for the richers test (first half chain up, other half chain down)
+    else:
+        dt_substep = dt_substep 
+        qc.x(range(half_N_sites)) # initial state for rog and vac osc tests
+
+    # transform basis from Z (up/down or |0>/|1>) to X (equal super position of |up>/ |down> to get the x basis =|+>/|->) using hadamard on the first qubit(b/c we are measurng the first qubit only)
+    # the measurement of counts in x basis is done later in main script to compute <sigma_x>
+    if measure == 'X':
+        qc.h(0)
+        
+    # prepare the quantum state by transforming basis from Z to Y using sdg on the first qubit(b/c we are measurng the first qubit only)
+    # Similarly, the 'Sdg' gate (S-dagger) is applied, which corresponds to the inverse of the phase gate 'S'. This introduces a phase shift of - pi/2.
+    # Then, a Hadamard gate 'H' is applied. This sequence of gates transforms from Z basis to Y basis. 
+    
+    # the measurement of counts in Y basis is done later in main script to compute <sigma_y>
+    elif measure == 'Y':
+        qc.sdg(0)
+        qc.h(0)
+
     for _ in range(num_steps):
         for coef, pauli in pauli_terms:
             #converts the Pauli operator object into its string representation
@@ -59,16 +114,16 @@ def evolve_and_measure_circuit(time, pauli_terms, N_sites,trotter_order):
                 apply_single_qubit_gate(qc, coef * dt_substep, qubits[0], pauli_str[qubits[0]])
             elif len(qubits) == 2:
                 apply_two_qubit_gate(qc, coef * dt_substep, qubits[0], qubits[1], pauli_str[qubits[0]], pauli_str[qubits[1]])
-
+                
     qc.measure(0, 0)
     return qc
+
 
 def evolve_and_measure_qcircuit(time, H, N_sites, measure='Z'):
     U = Operator(expm(-1j * H * time* 1/hbar))
     
     qc = QuantumCircuit(N_sites, 1)
     half_N_sites = N_sites // 2
-    # qc.x(range(half_N_sites))
     
     # Initialize the first half of the chain as up-spin (|0>)
     # Initialize the second half of the chain as down-spin (|1>)
