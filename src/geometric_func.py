25b544599010bed38d1271553149b0f84460ad7a
zlaraib <zlaraib@vols.utk.edu>
2025-08-31 14:33:25 -0400

    added theta_nu =1000 arbitrary for Josh test in geo_func

--- src/geometric_func.py
+++ src/geometric_func.py
@@ -5,5 +5,8 @@ def geometric_func(p, p_hat, i, j, theta_nu):
         return 1
     elif theta_nu in [0.1, 0, 0.01]:  # special addition for isotropic tests
         return 1
+    elif theta_nu==1000: # Josh test 
+        p[i, :] /= np.linalg.norm(p[i, :]) 
+        return 1 - np.dot(p[i], p[j])
     else:
         return 1 - np.dot(p_hat[i], p_hat[j])
