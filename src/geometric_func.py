3c5379d21b6b07f80a0389d5ca7a158a647403ea
zlaraib <zlaraib@vols.utk.edu>
2026-04-10 14:23:38 -0400

    fixed geometic src to remove magic number if-else conditions

--- src/geometric_func.py
+++ src/geometric_func.py
@@ -1,12 +1,11 @@
 import numpy as np
 
-def geometric_func(p, p_hat, i, j, theta_nu):
-    if np.array_equal(p[i], 0) and np.array_equal(p[j], 0.0):  # isotropic case
+def geometric_func(geometric_name, p_hat, i, j):
+    if geometric_name == "none":
         return 1
-    elif theta_nu in [0.1, 0, 0.01]:  # special addition for isotropic tests
-        return 1
-    elif theta_nu==1000: # Josh test 
-        p[i, :] /= np.linalg.norm(p[i, :]) 
-        return 1 - np.dot(p[i], p[j])
-    else:
+
+    elif geometric_name == "physical":
         return 1 - np.dot(p_hat[i], p_hat[j])
+
+    else:
+        raise ValueError("geometric_func not implemented")
